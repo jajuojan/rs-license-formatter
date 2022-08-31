82d3f7de3335c8c2832a12330577cab2eb64e756
Jani Ojanen <jajuojan@gmail.com>
2022-08-31 22:44:21 +0300

    test: Add basic tests

--- src/converter.rs
+++ src/converter.rs
@@ -50,3 +50,63 @@ impl PackageCollection {
         PackageCollection { packages, licenses }
     }
 }
+
+#[cfg(test)]
+pub mod tests {
+    use std::rc::Rc;
+
+    use crate::converter::{License, Package, PackageCollection};
+    use crate::readers::yaml_reader::tests::third_party_data;
+
+    pub fn package_collection_data() -> PackageCollection {
+        PackageCollection {
+            packages: vec![
+                Package {
+                    name: "my_dependency".to_owned(),
+                    version: "0.1.1".to_owned(),
+                    license: "MIT".to_owned(),
+                    licenses: vec![
+                        Rc::new(License {
+                            license: "MIT".to_owned(),
+                            text: "Lots of text...".to_owned(),
+                        }),
+                        Rc::new(License {
+                            license: "Apache-2.0".to_owned(),
+                            text: "Lots of text 2...".to_owned(),
+                        }),
+                    ],
+                },
+                Package {
+                    name: "my_other_dependency".to_owned(),
+                    version: "1.0.0".to_owned(),
+                    license: "Apache-2.0".to_owned(),
+                    licenses: vec![Rc::new(License {
+                        license: "Apache-2.0".to_owned(),
+                        text: "Lots of text 2...".to_owned(),
+                    })],
+                },
+            ],
+            licenses: vec![
+                Rc::new(License {
+                    license: "MIT".to_owned(),
+                    text: "Lots of text...".to_owned(),
+                }),
+                Rc::new(License {
+                    license: "Apache-2.0".to_owned(),
+                    text: "Lots of text 2...".to_owned(),
+                }),
+                Rc::new(License {
+                    license: "Apache-2.0".to_owned(),
+                    text: "Lots of text 2...".to_owned(),
+                }),
+            ],
+        }
+    }
+
+    #[test]
+    fn test_from_third_party() {
+        let input = third_party_data();
+        let output = PackageCollection::from_third_party(&input);
+        assert_eq!(output, package_collection_data(),);
+    }
+}
--- src/readers/yaml_reader.rs
+++ src/readers/yaml_reader.rs
@@ -28,3 +28,40 @@ pub fn read_from_file(file_name: &str) -> ThirdParty {
         serde_yaml::from_reader(file).expect("Unable to deserialize file contents");
     deserialized
 }
+
+#[cfg(test)]
+pub mod tests {
+    use super::{Library, License, ThirdParty};
+
+    pub fn third_party_data() -> ThirdParty {
+        ThirdParty {
+            root_name: "my-project".to_owned(),
+            third_party_libraries: vec![
+                Library {
+                    package_name: "my_dependency".to_owned(),
+                    package_version: "0.1.1".to_owned(),
+                    license: "MIT".to_owned(),
+                    licenses: vec![
+                        License {
+                            license: "MIT".to_owned(),
+                            text: "Lots of text...".to_owned(),
+                        },
+                        License {
+                            license: "Apache-2.0".to_owned(),
+                            text: "Lots of text 2...".to_owned(),
+                        },
+                    ],
+                },
+                Library {
+                    package_name: "my_other_dependency".to_owned(),
+                    package_version: "1.0.0".to_owned(),
+                    license: "Apache-2.0".to_owned(),
+                    licenses: vec![License {
+                        license: "Apache-2.0".to_owned(),
+                        text: "Lots of text 2...".to_owned(),
+                    }],
+                },
+            ],
+        }
+    }
+}
